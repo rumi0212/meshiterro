3e2542c75c1f9aa9f5d4817fdf4499b77373c00f
rumi0212 <106750897+rumi0212@users.noreply.github.com>
2022-06-25 08:51:17 +0000

    [Finish]4-8

---
+++ db/migrate/20220625083629_addshop_name_to_post_images.rb
@@ -0,0 +1,4 @@
+class AddshopNameToPostImages < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
---
+++ db/migrate/20220625084037_adduser_id_to_post_images.rb
@@ -0,0 +1,4 @@
+class AdduserIdToPostImages < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
