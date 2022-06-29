2a9b80f290fe38cb18892e88902ed57a98662ac8
rumi0212 <106750897+rumi0212@users.noreply.github.com>
2022-06-29 13:31:40 +0000

    [Finish]4-22

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
